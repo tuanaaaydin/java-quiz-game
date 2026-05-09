a80dbcf6fdee43ffd3669d1e11fd7e809f4bbf76
Tuana <tuana.aydin@icloud.com>
2026-05-09 15:54:09 +0000

    Quiz.java: Verweis auf umbenannte Klasse angepasst

--- Quiz.java
+++ Quiz.java
@@ -4,22 +4,18 @@ public class Quiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Frage[] fragen = {
-            new Frage("Was ist 7 + 5?", 12),
-            new Frage("Was ist 9 * 3?", 27),
-            new Frage("Was ist 100 - 44?", 56),
-            new Frage("Was ist 81 / 9?", 9)
+        QuizFrage[] fragen = {
+            new QuizFrage("Was ist 7 + 5?", 12),
+            new QuizFrage("Was ist 9 * 3?", 27),
+            new QuizFrage("Was ist 100 - 44?", 56),
+            new QuizFrage("Was ist 81 / 9?", 9)
         };
 
         int punkte = 0;
 
-        // alt:
-        System.out.println("=== Willkommen beim Zahlen-Quiz! ===");
-
-        // neu:
         System.out.println("=== Willkommen beim Mathe-Quiz! ===");
 
-        for (Frage frage : fragen) {
+        for (QuizFrage frage : fragen) {
             System.out.print(frage.getFrageText() + " ");
             int antwort = scanner.nextInt();
 
